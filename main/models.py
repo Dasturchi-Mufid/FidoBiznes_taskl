7fd0078a69662916f681db35f017865ad6529d27
Dasturchi Mufid <shahbozbekahmadaliyev@gmail.com>
2024-11-12 09:41:05 +0500

    test

--- main/models.py
+++ main/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     phone_number = models.CharField(max_length=15, unique=True)
 
-
     groups = models.ManyToManyField(
         'auth.Group',
         related_name='custom_user_set',
